e264e91cbc4d01ee1526072e468747aa04aba831
TheThomasY <twy1g16@soton.ac.uk>
2022-01-26 15:41:24 +0000

    added color array to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
+// prettier-ignore
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'body': '#17171F',
+        'selected-text': '#A3A3FF',
+        'theme': '#3F3FFF',
+        'nav': '#404053',
+        'secondary': '#9191A4',
+        'badge': '#3F3F51',
+        'input-border': '#565666',
+        'input': '#2A2A35',
+        'card': '#162a45',
+        'button-hover': '#2c2cb2',
+        'nav-bg': '#161b22'
+      },
+    },
   },
   plugins: [],
 }
