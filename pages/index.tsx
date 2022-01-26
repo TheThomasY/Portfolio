f8fd0f292a1f64e3e6be53913dd73ac29b0e1b90
TheThomasY <twy1g16@soton.ac.uk>
2022-01-26 16:45:25 +0000

    js added for side menu colour change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,6 @@
+// React
+import { useState, useEffect, useRef } from 'react';
+
 // Next
 import Head from 'next/head'
 
@@ -12,15 +15,49 @@ import SideMenu from '../components/SideMenu';
 
 
 export default function Home() {
+  // Array containing absolute Y position of home, work and contact sections respectively from the top of the view window.
+  const [sectionPositions, setSectionPositions] = useState([0, 0, 0]);
+
+  const sections = ['home', 'work', 'contact'];
+  const [currentSection, setCurrentSection] = useState(sections[0]);
+
+  const homeRef = useRef();
+  const workRef = useRef();
+  const contactRef = useRef();
+
+  function scrollHandler() {
+    setSectionPositions([
+      Math.abs(homeRef.current.getBoundingClientRect().top),
+      Math.abs(workRef.current.getBoundingClientRect().top),
+      Math.abs(contactRef.current.getBoundingClientRect().top),
+    ]);
+
+    setCurrentSection(
+      sections[sectionPositions.indexOf(Math.min(...sectionPositions))]
+    );
+  }
+
+  useEffect(() => {
+    function watchScroll() {
+      window.addEventListener('scroll', scrollHandler);
+    }
+    watchScroll();
+    return () => {
+      window.removeEventListener('scroll', scrollHandler);
+    };
+  });
   return (
     <div className="min-h-screen bg-body text-white pb-24">
       <Head>
         <title>Tom Young</title>
       </Head>
       <Navbar />
+      <div ref={homeRef}></div>
       <Hero />
-      <SideMenu />
+      <SideMenu current={currentSection}/>
+      <div ref={workRef}></div>
       <Projects />
+      <div ref={contactRef}></div>
       <Footer />
     </div>
   )
